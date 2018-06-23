9fa3b514f87230d62d91bea63dc4175d7c466e01
dummy <m.steve.jobs@me.com>
2018-06-23 21:21:39 +0800

    fix aggressive autocompletion

--- layers/+lang/html/funcs.el
+++ layers/+lang/html/funcs.el
@@ -18,4 +18,4 @@
 ;; see https://github.com/osv/company-web/issues/4
 (defun spacemacs//company-web-minimum-prefix-length ()
   "Set company minimum prefix length to 0 for the current buffer only."
-  (set (make-local-variable 'company-minimum-prefix-length) 0))
+  (set (make-local-variable 'company-minimum-prefix-length) 2))
